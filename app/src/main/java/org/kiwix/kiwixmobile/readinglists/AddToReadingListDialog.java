5c0b502542f5611901468354f5ecb98a0ac14133
Elad Keyshawn <elad.keyshawn@gmail.com>
2017-04-08 18:05:20 +0200

    added onboarding to readinglist dialog

--- app/src/main/java/org/kiwix/kiwixmobile/readinglists/AddToReadingListDialog.java
+++ app/src/main/java/org/kiwix/kiwixmobile/readinglists/AddToReadingListDialog.java
@@ -2,10 +2,11 @@ package org.kiwix.kiwixmobile.readinglists;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.InputType;
@@ -35,152 +36,181 @@ import butterknife.ButterKnife;
 public class AddToReadingListDialog extends ExtendedBottomSheetDialogFragment implements FastAdapter.OnClickListener<ReadingListItem> {
 
 
-    private FastAdapter<ReadingListItem> fastAdapter;
-    private ItemAdapter<ReadingListItem> itemAdapter;
-    private View listsContainer;
-    private CreateButtonClickListener createClickListener = new CreateButtonClickListener();
-    @Nullable
-    private DialogInterface.OnDismissListener dismissListener;
-//    private ReadingListItemCallback listItemCallback = new ReadingListItemCallback();
-    private String pageTitle;
-    private String pageUrl;
+  private FastAdapter<ReadingListItem> fastAdapter;
+  private ItemAdapter<ReadingListItem> itemAdapter;
+  private View mainContainer;
+  private CreateButtonClickListener createClickListener = new CreateButtonClickListener();
+  @Nullable
+  private DialogInterface.OnDismissListener dismissListener;
+  //    private ReadingListItemCallback listItemCallback = new ReadingListItemCallback();
+  private String pageTitle;
+  private String pageUrl;
 
-    private ReadingListFolderDao readinglistDao;
-    @BindView(R.id.snackbar_layout)
-    public FrameLayout snackbarLayout;
+  private ReadingListFolderDao readinglistDao;
+  @BindView(R.id.snackbar_layout)
+  public FrameLayout snackbarLayout;
 
-    private BookmarkArticle currentArticle;
+  private BookmarkArticle currentArticle;
+  private View onboardingButton;
+  private View onboardingContainer;
+  private ArrayList<ReadinglistFolder> folders;
 
 
-    public static AddToReadingListDialog newInstance(String articleTitle, @NonNull String articleUrl) {
-        return newInstance(articleTitle,articleUrl, null);
+  public static AddToReadingListDialog newInstance(String articleTitle, @NonNull String articleUrl) {
+    return newInstance(articleTitle, articleUrl, null);
+  }
+
+  public static AddToReadingListDialog newInstance(@NonNull String articleTitle, String articleUrl,
+                                                   @Nullable DialogInterface.OnDismissListener listener) {
+    AddToReadingListDialog dialog = new AddToReadingListDialog();
+    Bundle args = new Bundle();
+    args.putString("articleTitle", articleTitle);
+    args.putString("articleUrl", articleUrl);
+
+    dialog.setArguments(args);
+    dialog.setOnDismissListener(listener);
+    return dialog;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Bundle args = getArguments();
+    pageTitle = args.getString("articleTitle");
+    pageUrl = args.getString("articleUrl");
+    currentArticle = new BookmarkArticle(pageUrl, pageTitle);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+
+    readinglistDao = new ReadingListFolderDao(KiwixDatabase.getInstance(getActivity()));
+    folders = readinglistDao.getFolders();
+
+    View rootView = getActivity().getLayoutInflater().inflate(R.layout.dialog_add_to_readinglist, container);
+    onboardingContainer = rootView.findViewById(R.id.onboarding_container);
+    onboardingButton = rootView.findViewById(R.id.onboarding_button);
+    mainContainer = rootView.findViewById(R.id.dialog_add_to_readinglist_main_content_container);
+
+    RecyclerView readingListView = (RecyclerView) rootView.findViewById(R.id.list_of_lists);
+    fastAdapter = new FastAdapter<>();
+    itemAdapter = new ItemAdapter<>();
+
+    fastAdapter.withSelectOnLongClick(false);
+    fastAdapter.withSelectable(false);
+    fastAdapter.withOnClickListener(this);
+    readingListView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+    readingListView.setAdapter(itemAdapter.wrap(fastAdapter));
+
+    View createButton = rootView.findViewById(R.id.create_button);
+    createButton.setOnClickListener(createClickListener);
+
+    View closeButton = rootView.findViewById(R.id.close_button);
+    closeButton.setOnClickListener(v -> dismiss());
+
+    checkAndShowOnboarding();
+    updateLists();
+    return rootView;
+  }
+
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    ButterKnife.bind(getActivity());
+  }
+
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    return super.onCreateDialog(savedInstanceState);
+  }
+
+  @Override
+  public void dismiss() {
+    super.dismiss();
+    if (dismissListener != null) {
+      dismissListener.onDismiss(null);
     }
+  }
 
-    public static AddToReadingListDialog newInstance(@NonNull String articleTitle, String articleUrl,
-                                                     @Nullable DialogInterface.OnDismissListener listener) {
-        AddToReadingListDialog dialog = new AddToReadingListDialog();
-        Bundle args = new Bundle();
-        args.putString("articleTitle", articleTitle);
-        args.putString("articleUrl", articleUrl);
+  public void setOnDismissListener(DialogInterface.OnDismissListener listener) {
+    dismissListener = listener;
+  }
 
-        dialog.setArguments(args);
-        dialog.setOnDismissListener(listener);
-        return dialog;
+
+  private void updateLists() {
+    for (ReadinglistFolder folder : folders) {
+      itemAdapter.add(new ReadingListItem(folder.getFolderTitle()));
     }
+  }
 
+  @Override
+  public boolean onClick(View v, IAdapter<ReadingListItem> adapter, ReadingListItem item, int position) {
+    dismiss();
+    currentArticle.setParentReadinglist(item.getTitle());
+    readinglistDao.saveBookmark(currentArticle);
+    showAddedToListSnackbar();
+    return true;
+  }
+
+  private class CreateButtonClickListener implements View.OnClickListener {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Bundle args = getArguments();
-        pageTitle = args.getString("articleTitle");
-        pageUrl = args.getString("articleUrl");
-        currentArticle = new BookmarkArticle(pageUrl,pageTitle);
+    public void onClick(View v) {
+      dismiss();
+      showCreateListDialog();
     }
+  }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+  private void showCreateListDialog() {
+    new MaterialDialog.Builder(getActivity())
+        .title("Create a new reading list")
+        .content("Name your folder:")
+        .inputType(InputType.TYPE_CLASS_TEXT)
+        .input(null, "My readinglist", (dialog, input) -> {
+          ReadinglistFolder newReadinlistFolder = new ReadinglistFolder(input.toString());
+          readinglistDao.saveFolder(newReadinlistFolder);
+          currentArticle.setParentReadinglist(input.toString());
+          readinglistDao.saveBookmark(currentArticle);
+          showAddedToListSnackbar();
+        }).show();
 
-        readinglistDao = new ReadingListFolderDao(KiwixDatabase.getInstance(getActivity()));
-        View rootView = getActivity().getLayoutInflater().inflate(R.layout.dialog_add_to_readinglist, container);
-
-        listsContainer = rootView.findViewById(R.id.lists_container);
-
-        RecyclerView readingListView = (RecyclerView) rootView.findViewById(R.id.list_of_lists);fastAdapter = new FastAdapter<>();
-        itemAdapter = new ItemAdapter<>();
-
-        fastAdapter.withSelectOnLongClick(false);
-        fastAdapter.withSelectable(false);
-        fastAdapter.withOnClickListener(this);
-        readingListView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        readingListView.setAdapter(itemAdapter.wrap(fastAdapter));
-
-        View createButton = rootView.findViewById(R.id.create_button);
-        createButton.setOnClickListener(createClickListener);
-
-        View closeButton = rootView.findViewById(R.id.close_button);
-        closeButton.setOnClickListener(v -> dismiss());
-
-        updateLists();
-        return rootView;
-    }
+  }
 
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        ButterKnife.bind(getActivity());
-    }
+  private void checkAndShowOnboarding() {
+    SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
-    }
+    boolean isOnboarding = settings.getBoolean("runReadinglistDialogOnBoarding", true);
 
-    @Override
-    public void dismiss() {
-        super.dismiss();
-        if (dismissListener != null) {
-            dismissListener.onDismiss(null);
+    onboardingButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        onboardingContainer.setVisibility(View.GONE);
+        mainContainer.setVisibility(View.VISIBLE);
+
+        // set tutorial off
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("runReadinglistDialogOnBoarding",false);
+        editor.apply();
+        if (folders.isEmpty()) {
+          showCreateListDialog();
         }
-    }
-
-    public void setOnDismissListener(DialogInterface.OnDismissListener listener) {
-        dismissListener = listener;
-    }
-
-
-
-    private void updateLists() {
-        ArrayList<ReadinglistFolder> folders = readinglistDao.getFolders();
-        for (ReadinglistFolder folder: folders) {
-            itemAdapter.add(new ReadingListItem(folder.getFolderTitle()));
-        }
-    }
-
-    @Override
-    public boolean onClick(View v, IAdapter<ReadingListItem> adapter, ReadingListItem item, int position) {
-        dismiss();
-        currentArticle.setParentReadinglist(item.getTitle());
-        readinglistDao.saveBookmark(currentArticle);
-        showAddedToListSnackbar();
-        return true;
-    }
-
-    private class CreateButtonClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            dismiss();
-            showCreateListDialog();
-        }
-    }
-
-    private void showCreateListDialog() {
-        new MaterialDialog.Builder(getActivity())
-            .title("Create a new reading list")
-            .content("Name your folder:")
-            .inputType(InputType.TYPE_CLASS_TEXT)
-            .input(null, "My readinglist", (dialog, input) -> {
-                ReadinglistFolder newReadinlistFolder = new ReadinglistFolder(input.toString());
-                readinglistDao.saveFolder(newReadinlistFolder);
-                currentArticle.setParentReadinglist(input.toString());
-                readinglistDao.saveBookmark(currentArticle);
-                showAddedToListSnackbar();
-            }).show();
-
-    }
+      }
+    });
+    mainContainer.setVisibility(isOnboarding ? View.GONE : View.VISIBLE);
+    onboardingContainer.setVisibility(isOnboarding ? View.VISIBLE : View.GONE);
+  }
 
 
-
-    private void showAddedToListSnackbar() {
-        // TODO: after main activity's MVPed insert presenter on instantiation which will be able to trigger snackbar
+  private void showAddedToListSnackbar() {
+    // TODO: after main activity's MVPed insert presenter on instantiation which will be able to trigger snackbar
 //        Snackbar addedToListSnackbar =
 //            Snackbar.make(, "Article added to list", Snackbar.LENGTH_LONG);
 //        addedToListSnackbar.setActionTextColor(getResources().getColor(R.color.white));
 //        addedToListSnackbar.show();
-    }
-
+  }
 
 
 }
